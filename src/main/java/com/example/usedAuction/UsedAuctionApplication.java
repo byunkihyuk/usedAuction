d8d79aaedda0af374739496a82596d20d63eda68
byunkihyuk <rlgur109@naver.com>
2023-09-28 22:55:52 +0900

    Initial commit

---
+++ src/main/java/com/example/usedAuction/UsedAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.usedAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedAuctionApplication.class, args);
+	}
+
+}
