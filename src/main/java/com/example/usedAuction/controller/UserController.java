ea1e5dec64d3e50d72c8b0f9ca5c4eb52bd4b9b2
byunkihyuk <rlgur109@naver.com>
2023-10-22 15:47:33 +0900

    Feat: "로그인 기능 추가"

--- src/main/java/com/example/usedAuction/controller/UserController.java
+++ src/main/java/com/example/usedAuction/controller/UserController.java
@@ -1,18 +1,35 @@
 package com.example.usedAuction.controller;
 
+import com.example.usedAuction.config.jwt.JwtFilter;
+import com.example.usedAuction.config.jwt.TokenProvider;
+import com.example.usedAuction.dto.result.ResponseResult;
+import com.example.usedAuction.dto.user.UserSignInFormDto;
 import com.example.usedAuction.dto.user.UserSignUpFormDto;
 import com.example.usedAuction.service.UserService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
 
-    private final UserService userService;
+
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/sign-up")
     public ResponseEntity<Object> signUp(@RequestBody @Valid UserSignUpFormDto userSignUpFormDto){
@@ -27,5 +44,10 @@ public class UserController {
     @PostMapping("/sign-up/nicknameCheck")
     public String nicknameCheck(@RequestParam("nickname") String nickname){
         return userService.findByNickname(nickname);
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<Object> signIn(@RequestBody UserSignInFormDto userSignInFormDto){
+        return userService.signIn(userSignInFormDto);
     }
 }
