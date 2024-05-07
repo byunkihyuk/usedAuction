abcab4df973b13648865f29e740e5f1cf8626598
byunkihyuk <rlgur109@naver.com>
2024-05-07 18:23:48 +0900

    Add: "PayInfoDto 추가"

---
+++ src/main/java/com/example/usedAuction/dto/payment/PayInfoDto.java
@@ -0,0 +1,23 @@
+package com.example.usedAuction.dto.payment;
+
+import com.example.usedAuction.entity.transactionenum.TransactionRequestStateEnum;
+import com.example.usedAuction.entity.transactionenum.TransactionRequestTypeEnum;
+import com.example.usedAuction.entity.transactionenum.UsedTransactionTypeEnum;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class PayInfoDto {
+    private Integer payInfoId;
+    private Integer seller;
+    private Integer buyer;
+    private TransactionRequestTypeEnum transactionRequestType; // 충전 / 결제 / 출금 / 송금
+    private TransactionRequestStateEnum transactionRequestState; // 승인 / 대기 / 취소
+    private Integer transactionMoney; // 증감액
+    private UsedTransactionTypeEnum usedTransactionType; // 일반 거래글 거래 / 경매 거래글 입찰 / 입출금
+    private Integer generalTransactionId;
+    private Integer auctionTransactionId;
+    private Timestamp transactionTime;
+    private Timestamp transactionUpdateTime;
+}
