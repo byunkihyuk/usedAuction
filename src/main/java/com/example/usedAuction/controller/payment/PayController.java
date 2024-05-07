2574df526d22e67b1911f0aaa78ffb480bea7308
byunkihyuk <rlgur109@naver.com>
2024-05-07 22:33:20 +0900

    Feat: "머니 거래 내역 조회 기능 추가"

---
+++ src/main/java/com/example/usedAuction/controller/payment/PayController.java
@@ -0,0 +1,23 @@
+package com.example.usedAuction.controller.payment;
+
+import com.example.usedAuction.service.payment.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/pay")
+public class PayController {
+    private final PaymentService paymentService;
+
+    // 내역 조회
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity<Object> getHistory(@PathVariable Integer userId){
+        return paymentService.getHistory(userId);
+    }
+
+
+}
+
---
+++ src/main/java/com/example/usedAuction/service/payment/PaymentService.java
@@ -0,0 +1,53 @@
+package com.example.usedAuction.service.payment;
+
+import com.example.usedAuction.dto.DataMapper;
+import com.example.usedAuction.dto.payment.PayInfoDto;
+import com.example.usedAuction.dto.result.ResponseResult;
+import com.example.usedAuction.entity.user.User;
+import com.example.usedAuction.errors.ApiException;
+import com.example.usedAuction.errors.ErrorEnum;
+import com.example.usedAuction.repository.auction.AuctionBidRepository;
+import com.example.usedAuction.repository.auction.AuctionTransactionRepository;
+import com.example.usedAuction.repository.general.GeneralTransactionRepository;
+import com.example.usedAuction.repository.payment.PayInfoRepository;
+import com.example.usedAuction.repository.user.UserRepository;
+import com.example.usedAuction.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    private final PayInfoRepository payInfoRepository;
+    private final UserRepository userRepository;
+    private final GeneralTransactionRepository generalTransactionRepository;
+    private final AuctionTransactionRepository auctionTransactionRepository;
+    private final AuctionBidRepository auctionBidRepository;
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Object> getHistory(Integer userId) {
+        User loingUser = userRepository.findByUsername(SecurityUtil.getCurrentUsername().orElse(""))
+                .orElseThrow(()->new ApiException(ErrorEnum.NOT_FOUND_USER));
+
+        if(!Objects.equals(loingUser.getUserId(), userId)){
+            throw new ApiException(ErrorEnum.FORBIDDEN_ERROR);
+        }
+        List<PayInfoDto> payInfoDtoList = payInfoRepository.findAllBySellerOrBuyerOrderByTransactionUpdateTime(loingUser,loingUser).stream()
+                .map(DataMapper.instance::payInfoEntityToDto).collect(Collectors.toList());
+
+        ResponseResult<Object> result = new ResponseResult<>();
+        result.setStatus("success");
+        result.setData(payInfoDtoList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+
+}
