11f425caaf57018c5ab40a4d4807c9b0e895e7ee
byunkihyuk <rlgur109@naver.com>
2024-05-08 14:06:05 +0900

    Update: "User Entity Pessimistic lock 추가"
    1. Username으로 검색
    2. UserId로 검색

--- src/main/java/com/example/usedAuction/repository/user/UserRepository.java
+++ src/main/java/com/example/usedAuction/repository/user/UserRepository.java
@@ -2,7 +2,11 @@ package com.example.usedAuction.repository.user;
 
 import com.example.usedAuction.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
@@ -10,6 +14,16 @@ public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findOneWithAuthoritiesByUsername(String username);
 
     Optional<User> findByUsername(String username);
+
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select u from User u where u.username = :username")
+    Optional<User> findByUsernamePessimisticLock(@Param("username") String username);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select u from User u where u.userId = :userId")
+    Optional<User> findByUserIdPessimisticLock(@Param("userId") Integer userId);
+
 
     Optional<User> findByNickname(String nickname);
 }
