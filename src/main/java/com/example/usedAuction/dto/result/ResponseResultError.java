ffca19162b9dec42caba59e4f3dc52297feb18b4
byunkihyuk <rlgur109@naver.com>
2024-05-20 22:28:25 +0900

    Update: "ResponseResultError 객체 @NoArgsConstructor 추가"

--- src/main/java/com/example/usedAuction/dto/result/ResponseResultError.java
+++ src/main/java/com/example/usedAuction/dto/result/ResponseResultError.java
@@ -2,9 +2,11 @@ package com.example.usedAuction.dto.result;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ResponseResultError {
     private String status;
     private String message;
