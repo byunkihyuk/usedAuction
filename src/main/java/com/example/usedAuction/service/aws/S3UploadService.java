0cbc771aa9b43bdc7bb9f0ee05d385f68a564554
buynkihyuk <rlgur109@naver.com>
2024-09-22 17:38:11 +0900

    Update: "azure blob storage로 이미지 저장소 변경"

--- src/main/java/com/example/usedAuction/service/aws/S3UploadService.java
+++ src/main/java/com/example/usedAuction/service/aws/S3UploadService.java
@@ -1,45 +1,52 @@
 package com.example.usedAuction.service.aws;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobClientBuilder;
 import com.example.usedAuction.entity.TransactionImage;
-import com.example.usedAuction.entity.auction.AuctionTransactionImage;
-import com.example.usedAuction.entity.general.GeneralTransactionImage;
 import com.example.usedAuction.errors.ApiException;
 import com.example.usedAuction.errors.ErrorEnum;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class S3UploadService {
     private final AmazonS3 amazonS3;
-
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
-
     @Value("${cloud.aws.cloud-front-url}")
     private String cloudFrontUrl;
 
-    public String uploadImage(MultipartFile multipartFile,String imgName) {
+    @Value("${azure.connection-string}")
+    private  String connectionString;
+    @Value("${azure.container-name}")
+    private  String containerName;
 
-        ObjectMetadata meta = new ObjectMetadata();
-        meta.setContentType(multipartFile.getContentType());
-        meta.setContentLength(multipartFile.getSize());
+
+    public String uploadImage(MultipartFile multipartFile,String imgName) {
+        if(multipartFile.isEmpty()){
+            return null;
+        }
+
+        BlobClient blobClient = new BlobClientBuilder()
+                .connectionString(connectionString)
+                .containerName(containerName)
+                .blobName(imgName)
+                .buildClient();
+
         try {
-            amazonS3.putObject(bucket,imgName,multipartFile.getInputStream(),meta);
+            blobClient.upload(multipartFile.getInputStream(), multipartFile.getSize(), true);
         } catch (IOException e) {
             throw new ApiException(ErrorEnum.IMAGE_UPLOAD_ERROR);
         }
 
-        return cloudFrontUrl + amazonS3.getUrl(bucket,imgName).getPath();
+        return blobClient.getBlobUrl();
     }
 
     public boolean deleteImages(List<?> deleteImageList) {
@@ -48,12 +55,50 @@ public class S3UploadService {
         }
         int count = deleteImageList.size();
         for(Object obj : deleteImageList){
-            boolean isImage = amazonS3.doesObjectExist(bucket,  ((TransactionImage)obj).getImageName());
+            BlobClient blobClient = new BlobClientBuilder()
+                    .connectionString(connectionString)
+                    .containerName(containerName)
+                    .blobName(((TransactionImage)obj).getImageName())
+                    .buildClient();
+            boolean isImage = blobClient.exists();
             if (isImage) {
-                amazonS3.deleteObject(bucket,  ((TransactionImage)obj).getImageName());
+                try{
+                    blobClient.delete();
+                }catch (Exception e ){
+                    throw new ApiException(ErrorEnum.IMAGE_DELETE_ERROR);
+                }
                 count--;
             }
         }
         return count == 0;
     }
+
+//    public String uploadImage(MultipartFile multipartFile,String imgName) {
+//
+//        ObjectMetadata meta = new ObjectMetadata();
+//        meta.setContentType(multipartFile.getContentType());
+//        meta.setContentLength(multipartFile.getSize());
+//        try {
+//            amazonS3.putObject(bucket,imgName,multipartFile.getInputStream(),meta);
+//        } catch (IOException e) {
+//            throw new ApiException(ErrorEnum.IMAGE_UPLOAD_ERROR);
+//        }
+//
+//        return cloudFrontUrl + amazonS3.getUrl(bucket,imgName).getPath();
+//    }
+//
+//    public boolean deleteImages(List<?> deleteImageList) {
+//        if(deleteImageList.isEmpty()){
+//            return true;
+//        }
+//        int count = deleteImageList.size();
+//        for(Object obj : deleteImageList){
+//            boolean isImage = amazonS3.doesObjectExist(bucket,  ((TransactionImage)obj).getImageName());
+//            if (isImage) {
+//                amazonS3.deleteObject(bucket,  ((TransactionImage)obj).getImageName());
+//                count--;
+//            }
+//        }
+//        return count == 0;
+//    }
 }
